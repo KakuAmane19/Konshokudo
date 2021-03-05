0c7b55e178ffdd150d97ba3a823f640f0ce879b2
Amane Kaku <kakuamanefhafx@gmail.com>
2021-03-05 16:44:49 +0900

    十文字ハイライトどうやったら治る

--- src/app/pages/game/game.page.ts
+++ src/app/pages/game/game.page.ts
@@ -1,3 +1,4 @@
+import { CssSelector } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { timer } from 'rxjs';
@@ -10,22 +11,35 @@ const time$ = timer(3000);
   styleUrls: ['./game.page.scss'],
 })
 export class GamePage implements OnInit {
-  private selectedColor = false;
   public currentIndex: Array<number> = [-1, -1];
+  selectee = {
+    //filter: 'drop-shadow(0 0 0.75rem yellow)',
+    selectedColor: false,
+  };
 
   constructor(private router: Router) {}
 
   ngOnInit() {}
 
   ngDoCheck() {
-    console.log(this.currentIndex);
     //time$.subscribe(() => this.router.navigateByUrl('/result'));
   }
 
-  selectColor(i: number) {
-    this.currentIndex[0] === -1
-      ? (this.currentIndex[0] = i)
-      : (this.currentIndex[1] = i);
-    console.log(this.currentIndex);
+  selectColorY(i: number) {
+    this.currentIndex[1] = i;
+    this.selectee.selectedColor = this.selectee.selectedColor ? false : true;
+    console.log(this.currentIndex, this.selectee.selectedColor);
+  }
+
+  selectColorX(i: number) {
+    this.currentIndex[0] = i;
+  }
+
+  getColorY() {
+    return this.currentIndex[1];
+  }
+
+  getColorX(x: number) {
+    if (x === this.currentIndex[0]) return x;
   }
 }
