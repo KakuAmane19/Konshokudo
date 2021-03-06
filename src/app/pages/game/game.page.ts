db2b687c825e41a031ad2fb86c161978d015cf7a
Amane Kaku <kakuamanefhafx@gmail.com>
2021-03-06 22:38:09 +0900

    JSDocとコメントで、どういうメソッドを組み込もうかなぁという考えをかけるだけ書いた

--- src/app/pages/game/game.page.ts
+++ src/app/pages/game/game.page.ts
@@ -44,10 +44,58 @@ export class GamePage implements OnInit {
     //time$.subscribe(() => this.router.navigateByUrl('/result'));
   }
 
+  /**
+   * タイマーの表示
+   * @param
+   */
+
+  /**
+   * ラップタイムの記録＝＞サービス
+   * @param
+   */
+
+  /**
+   *今何問目？の更新とゲーム終了
+   * @param
+   */
+
+  /**
+   * 全色データ（サービス）から問題を受け取り、ランダムに必要数獲得・表示
+   * @param i
+   */
+
+  /**
+   * 出題色の生成
+   * @param i
+   */
+
+  /**
+   * 選択肢のうち、選ばれたオブジェクトのY座標を保管し、判定するメソッド
+   * QUESTION:値の確保と判定をこのコンポーネントのメソッド上で分けたほうがいい気がする……
+   * @param i 選択肢Y座標
+   * @return nothing
+   */
   selectColorY(i: number) {
     this.tempY = i;
 
-    //if:既に選択？
+    this.printJudge();
+
+    console.log(this.selectees, this.options);
+  }
+
+  /**
+   * 選択肢のうち、選ばれたオブジェクトのX座標を保管するメソッド
+   * @param i 選択肢X座標
+   * @return nothing
+   */
+  selectColorX(i: number) {
+    this.tempX = i;
+  }
+
+  /*******************************
+   * 選択された色を正誤判定サービスに渡す・及び正誤判定を表示するメソッド
+   */
+  printJudge(): void {
     if (this.options[this.tempY][this.tempX]) {
       this.options[this.tempY][this.tempX] = false;
       this.selectees[0][0] = -1;
@@ -59,7 +107,11 @@ export class GamePage implements OnInit {
       this.options[this.tempY][this.tempX] = true;
       this.selectees[1][0] = this.tempX;
       this.selectees[1][1] = this.tempY;
-      //答え合わせメソッドs
+
+      //答え合わせメソッド=>サービスに移乗
+
+      //答え合わせメソッドの返り値から、正誤判定表示
+
       for (let c = 0; c < this.options.length; c++) this.options[c].fill(false);
       for (let c = 0; c < this.selectees.length; c++)
         this.selectees[c].fill(-1);
@@ -68,11 +120,5 @@ export class GamePage implements OnInit {
       this.selectees[0][0] = this.tempX;
       this.selectees[0][1] = this.tempY;
     }
-
-    console.log(this.selectees, this.options);
-  }
-
-  selectColorX(i: number) {
-    this.tempX = i;
   }
 }
