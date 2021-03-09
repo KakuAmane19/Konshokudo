8c06d7dac5a60676854daf1dda9f7fd6e6e00036
Amane Kaku <kakuamanefhafx@gmail.com>
2021-03-09 18:48:28 +0900

    選択肢のと問題色の作成ができました

--- src/app/pages/game/game.page.ts
+++ src/app/pages/game/game.page.ts
@@ -1,7 +1,6 @@
-import { CssSelector } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { timer } from 'rxjs';
+import { timer, Observable } from 'rxjs';
 
 import { AllColorsService } from '../../services/all-colors.service';
 const time$ = timer(3000);
@@ -12,10 +11,13 @@ const time$ = timer(3000);
   styleUrls: ['./game.page.scss'],
 })
 export class GamePage implements OnInit {
+  //色オブジェクト
+
   //選択肢カラーオブジェクト5*5
-  public options = Array(5)
+  public selectedOptions = Array(5)
     .fill(0)
     .map(() => Array(5).fill(false));
+  public visibleOptions = Array(5).fill('');
 
   //選択している色の選択肢中の座標
   public selectees = Array(2)
@@ -25,6 +27,7 @@ export class GamePage implements OnInit {
   public tempX = -1;
 
   //問題カラーオブジェクト
+  public question = '';
 
   //回答カラーオブジェクト
 
@@ -36,6 +39,7 @@ export class GamePage implements OnInit {
 
   //その他デバッグ用変数
   public selectedColor = false;
+  private allColors;
 
   constructor(
     private router: Router,
@@ -44,6 +48,7 @@ export class GamePage implements OnInit {
 
   ngOnInit() {
     this.getOptions();
+    this.genQuestion();
   }
 
   ngDoCheck() {
@@ -66,17 +71,53 @@ export class GamePage implements OnInit {
    */
 
   /**
-   * 全色データ（サービス）から問題を受け取り、ランダムに必要数獲得・表示
-   * @param i
+   * 全色データ（サービス）から問題を受け取り表示
+   * @param nothing
    */
-  getOptions() {
-    console.log(this.allColorsService.provideColorData());
+  getOptions(): Promise<void> {
+    this.visibleOptions = this.allColorsService.provideOptions();
+    return;
   }
 
   /**
-   * 出題色の生成
-   * @param i
+   * 出題色の決定
+   * @param nothing
    */
+  genQuestion() {
+    //25色からランダムに2色取り出し
+    let pickedSource = [...Array(4)]
+      .fill(4.9)
+      .map((v) => Math.floor(Math.random() * v));
+
+    //hsl値の切り出し
+    let source1 = this.visibleOptions[pickedSource[0] * 5 + pickedSource[1]];
+    let source2 = this.visibleOptions[pickedSource[2] * 5 + pickedSource[3]];
+
+    //各値平均
+    let hsl1 = source1.split(/hsl\(|,|\%,|\%\)/g).slice(1, 4);
+    let hsl2 = source2.split(/hsl\(|,|\%,|\%\)/g).slice(1, 4);
+
+    let questionHSL = hsl1.map((v, i) => (parseInt(v) + parseInt(hsl2[i])) / 2);
+
+    //セット
+    this.question =
+      'hsl(' +
+      questionHSL[0] +
+      ',' +
+      questionHSL[1] +
+      '%,' +
+      questionHSL[2] +
+      '%)';
+    console.log(
+      pickedSource,
+      source1,
+      source2,
+      hsl1,
+      hsl2,
+      questionHSL,
+      this.question
+    );
+  }
 
   /**
    * 選択肢のうち、選ばれたオブジェクトのY座標を保管するメソッド
@@ -89,7 +130,7 @@ export class GamePage implements OnInit {
 
     this.printJudge();
 
-    console.log(this.selectees, this.options);
+    console.log(this.selectees, this.selectedOptions);
   }
 
   /**
@@ -105,15 +146,15 @@ export class GamePage implements OnInit {
    * 選択された色を検知し、正誤判定サービスに渡す、及び正誤判定を表示するメソッド
    */
   printJudge(): void {
-    if (this.options[this.tempY][this.tempX]) {
-      this.options[this.tempY][this.tempX] = false;
+    if (this.selectedOptions[this.tempY][this.tempX]) {
+      this.selectedOptions[this.tempY][this.tempX] = false;
       this.selectees[0][0] = -1;
       this.selectees[0][1] = -1;
     } else if (
       -1 < this.selectees[0][0] &&
-      !this.options[this.tempY][this.tempX]
+      !this.selectedOptions[this.tempY][this.tempX]
     ) {
-      this.options[this.tempY][this.tempX] = true;
+      this.selectedOptions[this.tempY][this.tempX] = true;
       this.selectees[1][0] = this.tempX;
       this.selectees[1][1] = this.tempY;
 
@@ -121,11 +162,12 @@ export class GamePage implements OnInit {
 
       //答え合わせメソッドの返り値から、正誤判定表示
 
-      for (let c = 0; c < this.options.length; c++) this.options[c].fill(false);
+      for (let c = 0; c < this.selectedOptions.length; c++)
+        this.selectedOptions[c].fill(false);
       for (let c = 0; c < this.selectees.length; c++)
         this.selectees[c].fill(-1);
     } else {
-      this.options[this.tempY][this.tempX] = true;
+      this.selectedOptions[this.tempY][this.tempX] = true;
       this.selectees[0][0] = this.tempX;
       this.selectees[0][1] = this.tempY;
     }
--- src/app/services/all-colors.service.ts
+++ src/app/services/all-colors.service.ts
@@ -4,33 +4,49 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class AllColorsService {
-  //色オブジェクト
-  private hueList: string[] = [
-    'red',
-    'magenta',
-    'blue',
-    'syan',
-    'green',
-    'yellow',
-  ];
-  private color = {
-    selected: false,
-    hue: '',
-    color: 'hsl(0,0,0)',
-  };
   //全色データ
-  private allColorsHSL = this.hueList.map((h, i) => [
-    h,
-    [...Array(6)].map((_, s) =>
-      [...Array(6)].map(
-        (_, l) => 'hsl(' + i * 30 + '%,' + s * 20 + '%,' + l * 20 + '%)'
+  private chromaticHSL = [...Array(6)].map((_, index) => [
+    [...Array(5)].map((_, s) =>
+      [...Array(5)].map(
+        (_, l) =>
+          'hsl(' +
+          index * 60 +
+          ',' +
+          (s + 1) * 20 +
+          '%,' +
+          ((l + 1) * 20 - 10) +
+          '%)'
       )
     ),
   ]);
+  private achromaticHSL = [...Array(5)].map(
+    (_, index) => 'hsl(' + 0 + ',' + 0 + '%,' + index * 20 + '%)'
+  );
 
+  private allColorsHSL = () => {
+    let linear = this.chromaticHSL.map((value, i) =>
+      value.reduce((pre, val) => pre.concat(val))
+    );
+    let linear2 = linear.reduce((pre, str) => pre.concat(str));
+    let linear3 = linear2.reduce((pre, str) => pre.concat(str));
+
+    return linear3.concat(this.achromaticHSL);
+  };
   constructor() {}
 
   provideColorData() {
-    return this.allColorsHSL;
+    return this.allColorsHSL();
+  }
+
+  provideOptions() {
+    let buffer = this.allColorsHSL();
+
+    //フィッシャーとイェーツによるアルゴリズム
+    for (let i = buffer.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * i);
+      buffer[i] = [buffer[j], (buffer[j] = buffer[i])][0];
+    }
+
+    return buffer.slice(0, 25);
   }
 }
